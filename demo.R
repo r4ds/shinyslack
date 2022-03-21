e9ed675b23db82a6e92c94c137d6ed811e61e119
Jon Harmon <jonthegeek@gmail.com>
2022-03-21 16:14:14 -0500

    Deploy to shinyapps.io

--- demo.R
+++ demo.R
@@ -1,18 +1,39 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
+
+# Switch this for shinyapps.
+site_url <- "http://127.0.0.1:4242/"
+# site_url <- "https://r4dscommunity.shinyapps.io/shinyslack/"
+
+team_id <- "T6UC1DKJQ"
+
+pkgload::load_all(
+  export_all = FALSE,
+  helpers = FALSE,
+  attach_testthat = FALSE,
+  quiet = TRUE
+)
+
 library(shiny)
-library(shinyslack)
 
 ui <- fluidPage(
   textOutput("user_name")
 )
 
 server <- function(input, output, session) {
-  team_id <- "T6UC1DKJQ"
   is_logged_in <- check_login(input, team_id)
 
   username <- shiny::reactive({
     shiny::req(is_logged_in())
     user_info <- slackcalls::post_slack(
       slack_method = "auth.test"
+    )
+
+    # Log the current user.
+    message(
+      "***** Login by: ",
+      user_info$user
     )
     user_info$user
   })
@@ -22,11 +43,10 @@ server <- function(input, output, session) {
   )
 }
 
-site_url <- "http://127.0.0.1:4242/"
 shiny::shinyApp(
   ui = slack_shiny_ui(
     ui,
-    team_id = "T6UC1DKJQ",
+    team_id = team_id,
     site_url = site_url
   ),
   server = server,
