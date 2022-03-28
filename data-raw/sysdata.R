3459807a279aa96f025555dad10c284242fb7001
Jon Harmon <jonthegeek@gmail.com>
2022-03-28 18:26:14 -0500

    Add login page.
    Closes #12.
    
    This is not perfect, but I want to get the code implemented so someone more UI-minded has something to work with.

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -5,10 +5,35 @@
 
 .shinyslack_nonce <- readRDS("data-raw/nonce.rds")
 
+.slack_logo_svg <- readLines("data-raw/slack_logo.svg")
+
+.slack_button_style <- paste(
+  sep = ";",
+  "align-items:center",
+  "color:#000",
+  "background-color:#fff",
+  "border:1px solid #ddd",
+  "border-radius:4px",
+  "display:inline-flex",
+  "font-family:Lato, sans-serif",
+  "font-size:18px",
+  "font-weight:600",
+  "height:56px",
+  "justify-content:center",
+  "text-decoration:none",
+  "width:296px"
+)
+
 usethis::use_data(
   .shinyslack_nonce,
+  .slack_logo_svg,
+  .slack_button_style,
   overwrite = TRUE,
   internal = TRUE
 )
 
-rm(.shinyslack_nonce)
+rm(
+  .shinyslack_nonce,
+  .slack_logo_svg,
+  .slack_button_style
+)
