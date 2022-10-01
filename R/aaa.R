bc354aa9ae54f71a797af4104962af84ac5b49c8
Jon Harmon <jonthegeek@gmail.com>
2022-10-01 14:08:38 -0500

    Abstract pieces out to {scenes}.

--- R/aaa.R
+++ R/aaa.R
@@ -1,11 +1,13 @@
 #' Parameters Used in Various Functions
 #'
+#' @param input The shiny input object.
 #' @param request The shiny request object to parse.
 #' @param site_url The URL of the Shiny app. This is used to return the user to
 #'   the app from the Slack login.
 #' @param team_id The Slack team ID through which the user is being
 #'   authenticated.
-#' @param input The shiny input object.
+#' @param ui A 0- or 1-argument function defining the UI of a Shiny app, or a
+#'   [shiny::tagList()].
 #' @name .shared-parameters
 #' @keywords internal
 NULL
